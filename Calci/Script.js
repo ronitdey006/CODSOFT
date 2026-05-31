23d03fac1b60f4ac7bc126cf7a6b1128600668d1
Ronit Dey <ronit.dey006@gmail.com>
2026-05-31 18:34:22 +0530

    Refactor calculator functions for better expression handling
    Refactor calculator logic to handle percentages and improve expression parsing.

--- Calci/Script.js
+++ Calci/Script.js
@@ -1,125 +1,85 @@
-var currentInput = "";
-var justCalculated = false;
+function safeMath(expression) {
+    expression = expression.replace(/(\d+(\.\d+)?)%/g, function(match, num) {
+        return "(" + num + "/100)";
+    });
 
-function appendToDisplay(value) {
-    var resultDiv = document.getElementById("result");
-    var expressionDiv = document.getElementById("expression");
+    var tokens = [];
+    var i = 0;
+    var expr = expression.replace(/\s/g, "");
 
-    if (justCalculated == true) {
-        if (value == "+" || value == "-" || value == "*" || value == "/" || value == "%") {
-            currentInput = resultDiv.innerText;
+    while (i < expr.length) {
+        var ch = expr[i];
+
+        if ((ch >= "0" && ch <= "9") || ch == ".") {
+            var num = "";
+            while (i < expr.length && ((expr[i] >= "0" && expr[i] <= "9") || expr[i] == ".")) {
+                num += expr[i];
+                i++;
+            }
+            tokens.push(parseFloat(num));
+            continue;
+        }
+
+        if (ch == "-" && (tokens.length == 0 || typeof tokens[tokens.length - 1] == "string")) {
+            var num = "-";
+            i++;
+            while (i < expr.length && ((expr[i] >= "0" && expr[i] <= "9") || expr[i] == ".")) {
+                num += expr[i];
+                i++;
+            }
+            tokens.push(parseFloat(num));
+            continue;
+        }
+
+        if (ch == "+" || ch == "-" || ch == "*" || ch == "/") {
+            tokens.push(ch);
+            i++;
+            continue;
+        }
+
+        if (ch == "(") {
+            var depth = 1;
+            var inner = "";
+            i++;
+            while (i < expr.length && depth > 0) {
+                if (expr[i] == "(") depth++;
+                if (expr[i] == ")") depth--;
+                if (depth > 0) inner += expr[i];
+                i++;
+            }
+            tokens.push(safeMath(inner));
+            continue;
+        }
+
+        i++;
+    }
+
+    var i2 = 1;
+    while (i2 < tokens.length - 1) {
+        if (tokens[i2] == "*") {
+            var result = tokens[i2 - 1] * tokens[i2 + 1];
+            tokens.splice(i2 - 1, 3, result);
+        } else if (tokens[i2] == "/") {
+            if (tokens[i2 + 1] == 0) return NaN;
+            var result = tokens[i2 - 1] / tokens[i2 + 1];
+            tokens.splice(i2 - 1, 3, result);
         } else {
-            currentInput = "";
-            expressionDiv.innerText = "";
-        }
-        justCalculated = false;
-    }
-
-    if (value == ".") {
-        var parts = currentInput.split(/[\+\-\*\/]/);
-        var lastPart = parts[parts.length - 1];
-        if (lastPart.includes(".")) {
-            return;
+            i2 += 2;
         }
     }
 
-    var operators = ["+", "-", "*", "/", "%"];
-    var lastChar = currentInput[currentInput.length - 1];
-    if (operators.includes(value) && operators.includes(lastChar)) {
-        currentInput = currentInput.slice(0, -1);
-    }
-
-    currentInput = currentInput + value;
-    expressionDiv.innerText = currentInput;
-    resultDiv.innerText = currentInput;
-}
-
-function calculate() {
-    var resultDiv = document.getElementById("result");
-    var expressionDiv = document.getElementById("expression");
-
-    if (currentInput == "") {
-        return;
-    }
-
-    try {
-        // Replace % with /100 for percentage
-        var expression = currentInput.replace(/(\d+\.?\d*)%/g, function(match, num) {
-            return "(" + num + "/100)";
-        });
-
-        // Use Function constructor instead of eval
-        var answer = new Function("return " + expression)();
-
-        if (answer == undefined || isNaN(answer) || !isFinite(answer)) {
-            resultDiv.innerText = "Error";
-            return;
-        }
-
-        if (answer % 1 !== 0) {
-            answer = parseFloat(answer.toFixed(8));
-        }
-
-        expressionDiv.innerText = currentInput + " =";
-        resultDiv.innerText = answer;
-        currentInput = answer.toString();
-        justCalculated = true;
-
-    } catch (error) {
-        resultDiv.innerText = "Error";
-        expressionDiv.innerText = "";
-        currentInput = "";
-    }
-}
-
-function clearAll() {
-    currentInput = "";
-    justCalculated = false;
-    document.getElementById("result").innerText = "0";
-    document.getElementById("expression").innerText = "";
-}
-
-function deleteLast() {
-    if (justCalculated == true) {
-        clearAll();
-        return;
-    }
-
-    if (currentInput.length > 0) {
-        currentInput = currentInput.slice(0, -1);
-        var resultDiv = document.getElementById("result");
-        var expressionDiv = document.getElementById("expression");
-        expressionDiv.innerText = currentInput;
-        if (currentInput == "") {
-            resultDiv.innerText = "0";
+    var i3 = 1;
+    while (i3 < tokens.length - 1) {
+        if (tokens[i3] == "+") {
+            var result = tokens[i3 - 1] + tokens[i3 + 1];
+            tokens.splice(i3 - 1, 3, result);
+        } else if (tokens[i3] == "-") {
+            var result = tokens[i3 - 1] - tokens[i3 + 1];
+            tokens.splice(i3 - 1, 3, result);
         } else {
-            resultDiv.innerText = currentInput;
+            i3 += 2;
         }
     }
-}
 
-document.addEventListener("keydown", function(event) {
-    var key = event.key;
-    if (key >= "0" && key <= "9") {
-        appendToDisplay(key);
-    } else if (key == "+") {
-        appendToDisplay("+");
-    } else if (key == "-") {
-        appendToDisplay("-");
-    } else if (key == "*") {
-        appendToDisplay("*");
-    } else if (key == "/") {
-        event.preventDefault();
-        appendToDisplay("/");
-    } else if (key == "%") {
-        appendToDisplay("%");
-    } else if (key == ".") {
-        appendToDisplay(".");
-    } else if (key == "Enter" || key == "=") {
-        calculate();
-    } else if (key == "Backspace") {
-        deleteLast();
-    } else if (key == "Escape") {
-        clearAll();
-    }
-});
+    return tokens[0];
+}
